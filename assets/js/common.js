8374386198902d72945d62c5b32324aec81da85f
zengchen <zengchen98@163.com>
2017-04-18 23:05:07 +0800

    进行登录验证

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,8 +1,16 @@
 
-	// NProgress.start();
+// NProgress.start();
 
-	// NProgress.done();
+// NProgress.done();
 
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+// 课程管理子菜单显示隐藏
+$('.navs ul').prev('a').on('click', function () {
+	$(this).next().slideToggle();
+});
+
+
+// 登录验证:
+// 判断用户是否登录过, 如果登录过, 就什么也不做
+// 如果没有登录过,
+// 用户访无论访问哪个页面(除登录页),先跳转到登录页面
+
